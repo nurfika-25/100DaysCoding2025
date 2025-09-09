365f760912d0be468455e9cdde4c07c9b82e769f
nurfika-25 <nurfikaika25@gmail.com>
2025-09-09 18:09:40 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,19 @@
+public class fika_D0225006 {
+    public static void main(String[] args) {
+
+        System.out.println("Nama: Nurfika Rusdi");
+        System.out.println("Umur: 18 tahun");
+        System.out.println("Prodi: Informatika");
+        
+
+        System.out.print("Haii!! nama saya fika, ");
+        System.out.print("Umur 18 tahun kelas A Informatika");
+
+      
+        System.out.printf("Nama:%s %nUmur:%d Prodi%s %nberat badan: %.1f", "Nurfika Rusdi",18,"Informatika",43.5);
+        System.out.println();
+        
+    }
+    
+    
+}
