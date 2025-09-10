669b3239f431f9dfeab4ef889ad0797755016d6c
nurfika-25 <nurfikaika25@gmail.com>
2025-09-10 15:37:15 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,16 @@
+public class fikaa {
+    public static void main(String[] args) {
+// penggunaan println digunakan untuk menampilkan teks di konsol dan berpindah ke baris baru
+        System.out.println("Nama : Nurfika Rusdi");
+        System.out.println("Kelas : Informatika A");
+        System.out.println("Alamat : Katitting");
+      
+// penggunaan print digunakan untuk menampilkan teks pada konsol untuk menyambungkan kalimat tanpa baris baru
+        System.out.print("Hello everyone perkenalkan nama saya Nurfika Rusdi kalian bisa memanggil saya fika,");
+        System.out.print("umur saya 18 tahun saya berasal dari katitting dan alumni dari UPTD SMAN 2 Majene");
+      
+// penggunaan printf digunakan untuk membuat format seperti angka atau bilangan dan sebagainya yang lebih fleksibel dan berurut tetapi menambahkan %
+        System.out.printf("%nNama : %s %nProdi: %s %njumlah sodarah :%d %n","Nurfika Rusdi" , "Informatika" , 5 );
+        System.out.println();
+    }
+}
