8a0b8d7cc83a8ed5fa54ff740b17b2a1e8acf787
nurfika-25 <nurfikaika25@gmail.com>
2025-09-11 00:54:08 +0800

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -1,9 +1,9 @@
 public class fikaaa {
-    public static void main(String[] args) {
+    public static void main(String[] args) {       
         String nama,alamat,hobi;
         nama = "fika";
         alamat = "Katitting";
-        hobi ="nonton drakor";
+        hobi ="nonton drakor dan membaca";
         int umur = 18;
         
         System.out.printf("\n nama saya \t\t:" + nama);
@@ -12,9 +12,9 @@ public class fikaaa {
         System.out.printf("\n umur saya  \t\t:" + 18 );
         System.out.println("");
         
-//   escape squence "\n" digunakan ketika kita ingin memakai 
-//   satu sout saja tetapi ingin dijadikan garis baru.
-//   sedangkan escape squence "\t" digunakan untuk tab atau 
-//   untuk merapihkan dan mensejajarkan baris inputan yang berantakan.
+//  escape squence "\n" digunakan ketika kita ingin memakai 
+//  satu sout saja tetapi ingin dijadikan garis baru.
+//  sedangkan escape squence "\t" digunakan untuk tab atau 
+//  untuk merapihkan dan mensejajarkan baris inputan yang berantakan.
     }
-    }
+}
