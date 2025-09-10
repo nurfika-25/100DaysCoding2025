5d574eca047d59f5c285abed733fb61dc206c4e6
nurfika-25 <nurfikaika25@gmail.com>
2025-09-11 00:45:42 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,19 @@
+public class fikaaa {
+    public static void main(String[] args) {
+        String nama,alamat,hobi;
+        nama = "fika";
+        alamat = "Katitting";
+        hobi ="nonton drakor";
+        int umur = 18;
+        
+        System.out.printf("\n nama saya \t\t:" + nama);
+        System.out.printf("\n alamat saya di \t:" + alamat );
+        System.out.printf("\n hobi saya \t\t:" + hobi );
+        System.out.printf("\n umur saya  \t\t:" + 18 );
+        System.out.println("");
+//        escape squence "\n" digunakan ketika kita ingin memakai 
+//        satu sout saja tetapi ingin dijadikan garis baru.
+//        sedangkan escape squence "\t" digunakan untuk tab atau 
+//        untuk merapihkan dan mensejajarkan baris inputan yang berantakan.
+    }
+    }
