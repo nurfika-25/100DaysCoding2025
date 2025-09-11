9adfb695e2bc785d4326cc61189d277e4f679462
nurfika-25 <nurfikaika25@gmail.com>
2025-09-11 13:35:21 +0800

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -11,7 +11,7 @@ public class fikaaa {
         System.out.printf("\n hobi saya \t\t:%s", hobi );
         System.out.printf("\n umur saya  \t\t:" + 18 );
         System.out.println("");
-        
+    
 //  escape squence "\n" digunakan ketika kita ingin memakai 
 //  satu sout saja tetapi ingin dijadikan garis baru.
 //  sedangkan escape squence "\t" digunakan untuk tab atau 
