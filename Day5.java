ae86385d3e579cfebf13896e2cd6256067e3d1ad
nurfika-25 <nurfikaika25@gmail.com>
2025-09-12 18:32:13 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,12 @@
+public class fikaaa {
+    public static void main(String[] args) {      
+        byte angka1 = 125; 
+        short angka2 = 25700;
+        int angka3 = 210030505;
+        long angka4 = 1000000000l;
+        System.out.println("Tipe data byte: "+angka1);
+        System.out.println("Tipe data short: "+angka2);
+        System.out.println("Tipe data integer: "+angka3);
+        System.out.println("Tipe data long: "+angka4);
+    } 
+}
