e1fa6cfb42c03c0da3efc7508cbf4f503592158f
nurfika-25 <nurfikaika25@gmail.com>
2025-09-13 13:03:45 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class fikaaa {
+    public static void main(String[] args) {      
+        double tinggi = 153.5;
+        float kg = 43.5f;
+        System.out.println("Tinggi saya: "+tinggi);
+        System.out.println("Berat badan saya adalah "+kg+" kilogram");
+
+    }
+}
