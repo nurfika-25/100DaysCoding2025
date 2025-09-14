fc9e36a0ec0586cf5b9a270b7f25e58bb44e9822
nurfika-25 <nurfikaika25@gmail.com>
2025-09-14 20:48:58 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,7 @@
+public class fikaaa {
+    public static void main(String[] args) {      
+        char inisial = 'K';
+        boolean asal = true;
+        System.out.println("Inisial asal saya yaitu" + " " +inisial+ " " + "Katitting");
+    }
+}
