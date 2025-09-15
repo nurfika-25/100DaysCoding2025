40292a9a09e1512250be11cd73a881a4f05fdbbf
nurfika-25 <nurfikaika25@gmail.com>
2025-09-15 19:48:23 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,18 @@
+//String merupakan tipe data non-primitif
+//String bisa digunakan untuk teks maupun angka selama masih di dalam tanda petik dua.
+// String nama,nim,kelas,prodi,day coding,alamat,asal sekolah;
+        String nama = "Nurfika Rusdi";
+        String nim = "D0225006";
+        String prodi = "Iformatika A";
+        String DayNgoding = "day8";
+        String AsalSekolah = "SMAN 2 MAJENE";
+        String alamat = "Katitting";
+
+        System.out.println("Perkenalkan nama saya\t: "+nama);
+        System.out.println("NIM saya\t\t: "+nim);
+        System.out.println("Saya berasan dari prodi: "+prodi);
+        System.out.println("Hari ngoding\t\t: "+DayNgoding);
+        System.out.println("Asal sekolsh saya\t: "+AsalSekolah);
+        System.out.println("alamat saya di\t\t:"+alamat);
+    }
+}
