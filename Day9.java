befc9de7e138a63aa61897ee4cf32188f7bb3b51
nurfika-25 <nurfikaika25@gmail.com>
2025-09-16 18:28:31 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,34 @@
+public class fika {
+    public static void main(String[] args) {
+        
+//Deklarasi
+        String Nama,Kelas,Alamat;
+        int Umur,DayNgoding;
+        double Tinggi,BeratBadan;
+        
+//Inisialisasi
+        Nama = "fika";
+        Kelas= "Informatika";
+        Alamat= "Polewali";
+        Umur = 16;
+        DayNgoding=8;
+        Tinggi = 150.0;
+        BeratBadan = 43.0;
+        
+//Update variabel
+        Nama = "Nurfika Rusdi";
+        Kelas= "Informatika A";
+        Alamat= "Katitting";
+        Umur = 17;
+        DayNgoding=9;
+        Tinggi = 153.5;
+        BeratBadan = 43.5;
+        System.out.println("Nama saya: "+Nama);
+        System.out.println("kelas saya: "+Kelas);
+        System.out.println("Saya berasal dari: "+Alamat);
+        System.out.println("Saya berumur: "+Umur);
+        System.out.println("hari ngoding: "+DayNgoding);
+        System.out.println("Tinggi saya: "+Tinggi);
+        System.out.println("Berat Badan saya: "+BeratBadan);
+    }
+}
