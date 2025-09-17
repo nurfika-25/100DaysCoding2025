a27d6e1110d97fe17818f2a4acedad1ecc1fc521
nurfika-25 <nurfikaika25@gmail.com>
2025-09-17 20:04:33 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,18 @@
+public class fika {
+    public static void main(String[] args) {
+        
+        final String nama= "Nurfika";
+        final String kelas= "Informatika A";
+        final String hobi = "Nonton drakor";
+        final byte umur = 17;
+        final int tinggi = 153;
+        final double bb = 43.5;
+        System.out.println("Nama : "+nama);
+        System.out.println("kelas saya : "+kelas);
+        System.out.println("hobi saya: "+hobi);
+        System.out.println("Umur saya: "+umur);
+        System.out.println("Tinggi badan saya: "+tinggi);
+        System.out.println("Berat badan saya: "+bb);
+
+    }
+}
