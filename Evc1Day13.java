e90fcdc44182858555d8a69789b7b6288250a43b
nurfika-25 <nurfikaika25@gmail.com>
2025-09-21 09:55:56 +0800

    Create Evc1Day13.java

---
+++ Evc1Day13.java
@@ -0,0 +1,91 @@
+package Fika_D0225006;
+
+public class fika {
+    public static void main(String[] args) { 
+    //membuat 9 tipe data memiliki 2 variabel
+        String Nama,Alamat;
+        int umur = 2;
+        int JumlahSaudara = 3;
+        long a = 123456;
+        long b = 567890;
+        byte nilaiA = 50;
+        byte nilaiB = 90;
+        short luas = 235;
+        short tinggi = 250;
+        double BB = 40.5;
+        double Tb = 151.5;
+        float fik1 = 20.3f;
+        float fik2 = 02.8f;
+        char huruf1 = 'f';
+        char huruf2 = 'a';
+        String name = "Nirfika";
+        String alamat= "tinambung";
+        boolean benar = true;
+        boolean salah = false;
+        
+        
+        System.out.println("Sebelum update");
+        System.out.println(umur);
+        System.out.println(JumlahSaudara);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(nilaiA);
+        System.out.println(nilaiB);
+        System.out.println(luas);
+        System.out.println(tinggi);
+        System.out.println(BB);
+        System.out.println(Tb);
+        System.out.println(fik1);
+        System.out.println(fik2);
+        System.out.println(huruf1);
+        System.out.println(huruf2);
+        System.out.println(name);
+        System.out.println(alamat);
+        System.out.println(benar);
+        System.out.println(salah);
+        System.out.println();
+        
+     
+        
+        umur = 18;
+        JumlahSaudara = 5;
+        a = 051024;
+        b = 250807;
+        nilaiA = 25;
+        nilaiB = 28;
+        luas = 32;
+        tinggi =82 ;
+        BB = 43.5;
+        Tb = 153.5;
+        fik1 = 25.3f;
+        fik2 = 8.2f;
+        huruf1 = 'n';
+        huruf2 = 's';
+        name = "Nurfika Rusdi";
+        alamat = "Katitting";
+        
+        
+        
+        //cetak dengan print,println,printf
+        System.out.println("Sesudah update");
+        System.out.println(umur);
+        System.out.println(JumlahSaudara);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(nilaiA);
+        System.out.println(nilaiB);
+        System.out.println(luas);
+        System.out.println(tinggi);
+        System.out.println(BB);
+        System.out.println(Tb);
+        System.out.println(fik1);
+        System.out.println(fik2);
+        System.out.println(huruf1);
+        System.out.println(huruf2);
+        System.out.printf(("%s"),name);
+        System.out.printf(("\n%s"),alamat);
+        System.out.print("\n"+benar);
+        System.out.print("\n"+salah);
+     
+    }
+}
