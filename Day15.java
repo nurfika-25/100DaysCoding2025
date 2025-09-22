b56dc8e3092e724f0f6c4f5e51bab2739a04b994
nurfika-25 <nurfikaika25@gmail.com>
2025-09-22 22:06:10 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,18 @@
+package Fika_D0225006;
+import java.util.Scanner;
+public class fikaa {
+    public static void main(String[] args) {
+       Scanner in = new  Scanner(System.in);
+        System.out.print("Masukkan angka pertama: ");
+        int angka1 = in.nextInt();
+        System.out.print("Masukkan angka kedua: ");
+        int angka2 = in.nextInt();
+        System.out.println();
+        System.out.println("Hasil penjumlahan dari " + ("angka1 dan angka2 adalah ")+ (angka1 + angka2));
+        System.out.println("Hasil pengurangan dari " + ("angka1 dan angka2 adalah ")+ (angka1 - angka2) );                            
+        System.out.println();
+
+    
+    }
+    
+}
