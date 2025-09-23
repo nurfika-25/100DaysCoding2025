5b232c8dbc5349b8fd4495c721cf8e506968357b
nurfika-25 <nurfikaika25@gmail.com>
2025-09-23 23:18:09 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,20 @@
+package Fika_D0225006;
+import java.util.Scanner;
+public class fikaa {
+    public static void main(String[] args) {
+       Scanner in = new  Scanner(System.in);
+        System.out.print("Masukkan angka pertama : ");
+        double fi1 = in.nextDouble();
+        System.out.print("Masukkan  angka kedua  : ");
+        double fi2 = in.nextDouble();
+        System.out.println();
+        System.out.println("Hasil perkalian dari fi1 dan fi2 " + ("adalah ")+ (fi1 * fi2));
+        System.out.println("Hasil pembagian dari fi1 dan fi2 " + ("adalah ")+ (fi1 / fi2)); 
+        
+        System.out.println();
+
+    
+    }
+    
+}
+        
