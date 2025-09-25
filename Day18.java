1346b575997984bc2a033f952cf9480a6137ccd6
nurfika-25 <nurfikaika25@gmail.com>
2025-09-25 19:00:51 +0800

    Update Day18.java

--- Day18.java
+++ Day18.java
@@ -1,13 +1,9 @@
 package Fika_D0225006;
-import java.util.Scanner;
 public class fikaa {
     public static void main(String[] args) {
-       Scanner in = new  Scanner(System.in);
        byte a = 127;
        short b = a;
         System.out.println(b);    
     }
     
 }
-        
-   
