27458362536a9b17a94df5078dec8965d7098710
nurfika-25 <nurfikaika25@gmail.com>
2025-09-26 22:35:54 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,10 @@
+package Fika_D0225006;
+public class fikaa {
+    public static void main(String[] args) {       
+       long x = 2537;
+       byte y = (byte) x; // paksa konversi dari long ke byte
+        System.out.println("Konversi paksa: "+ y);
+        System.out.println("");
+     
+    }
+     }
