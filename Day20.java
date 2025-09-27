2d19dbf1e97802d7f6fc45bf5d91b0d85db75d5b
nurfika-25 <nurfikaika25@gmail.com>
2025-09-27 14:54:59 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class fikaa {
+    public static void main(String[] args) {
+//       Mengubah tipe data primitif ke String 
+     //Mengubah tipe data string menjadi primitif
+//     int ke String  
+        int fi = 25032007;
+        String fika = Integer.toString(fi);
+        System.out.println("integer -> String yaitu "+ fi);
+//     double ke String
+        double saaa = 25.3;
+        String aksa = Double.toString(saaa);
+        System.out.println("double -> String yaitu "+ saaa);
+    }
+    }
