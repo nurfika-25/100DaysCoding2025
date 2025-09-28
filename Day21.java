49505af971306536bd91e0ac4ba7694dd0926f4d
nurfika-25 <nurfikaika25@gmail.com>
2025-09-28 13:45:31 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,15 @@
+package Nurfika_D0225006;
+public class Fika_20 {
+    public static void main(String[] args) {
+        long x = 2007;
+        int  y = 2006;
+        x=(short) (x+y); 
+        y=(short) (x-y); 
+        x=(short) (x-y); 
+        System.out.println(" jadi nilai x = "+x);
+        System.out.println(" dan nilai  y = "+y);
+
+
+    }
+   
+}
