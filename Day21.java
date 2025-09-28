fadedd9b3fcfdc9b52dfcc15652b4a95a147fe18
nurfika-25 <nurfikaika25@gmail.com>
2025-09-28 14:16:50 +0800

    Day21.java

--- Day21.java
+++ Day21.java
@@ -1,11 +1,12 @@
 package Nurfika_D0225006;
 public class Fika_20 {
     public static void main(String[] args) {
-        long x = 2007;
-        int  y = 2006;
-        x=(short) (x+y); 
-        y=(short) (x-y); 
-        x=(short) (x-y); 
+        int x = 27;
+        int  y = 26;
+        x=(x+y); 
+        y=(x-y); 
+        x=(x-y); 
+//  kita tukar nilai x dan y    
         System.out.println(" jadi nilai x = "+x);
         System.out.println(" dan nilai  y = "+y);
 
