92578e7ccfda953e2a51833957088b0610dce7b9
nurfika-25 <nurfikaika25@gmail.com>
2025-09-29 22:38:47 +0800

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class fika123 {
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in); 
+        System.out.println("Masukkan sisi: ");
+        double sisi = in.nextDouble();
+        double luas = sisi*sisi;
+        System.out.println("Jadi luas persegi adalah : "+ luas);
+                  
+    }
+     
+}
