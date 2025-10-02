d97d1d9368788d9c559db3cd740ac1bcaacf0606
nurfika-25 <nurfikaika25@gmail.com>
2025-10-02 20:39:20 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class fika123 {
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       int fi = 5;       
+        fi++;
+        System.out.println("Hasil increment: "+fi);
+        fi--;
+        System.out.println("Hasil Decrement: "+fi); //Hasil nya akan kembali ke nilai inisialisasi awal 
+
+    }
+}
