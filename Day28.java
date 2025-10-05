293e76c0ee40d13799d0d431db55a685b42da79b
nurfika-25 <nurfikaika25@gmail.com>
2025-10-05 21:45:44 +0800

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class fika123 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int x = in.nextInt();
+        System.out.print("Masukkan angka: ");
+        int y = in.nextInt();        
+        System.out.println("Apakah nilai x sama dengan y? :"+(x==y));
+        System.out.println("Apakah nilai y tidak sama dengan y? :"+(x!=y));
+        
+    }
+}
