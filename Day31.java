c0835a68ac33282405b460d8a08dcb7894a8794b
nurfika-25 <nurfikaika25@gmail.com>
2025-10-08 23:10:35 +0800

    Day31.java

--- Day31.java
+++ Day31.java
@@ -7,7 +7,7 @@ public class fika123 {
         System.out.print("Masukkan angka: ");
         double y = in.nextDouble();
         
-        boolean p = x>y && y<=x; 
+        boolean p = x>=y && y<=x; 
         boolean q = x<=y && y>=x;
         System.out.println("Apakah nilai x lebih besar sama dengan y dan apakah nilai y lebih kecil sama dengan x?: " +p);
         System.out.println("Apakah nilai x lebih kecil sama dengan y dan apakah nilai y lebih besar sama dengan x?: " +q);
