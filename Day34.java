1e2f6bb906f8b5e893198be4decbf1ce44378a51
nurfika-25 <nurfikaika25@gmail.com>
2025-10-11 20:53:57 +0800

    Day34.java

---
+++ Day34.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class fikaayo {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.print("Nilai UTS: ");
+        int a= in.nextInt();
+        System.out.print("Nilai UAS: ");
+        int b= in.nextInt();
+        if (a >= 85 && b>= 80){
+            System.out.println("Selamat, anda lulus");
+        }else if (a >=70 || b >=70){
+            System.out.println("Maaf, anda belum lulus");
+        }else {
+            System.out.println("Persiapkan diri anda kembali di tahun depan");
+        }
+    }
+}
