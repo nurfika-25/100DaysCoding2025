91405231aec743f18476817f45026a8eb5f32808
nurfika-25 <nurfikaika25@gmail.com>
2025-10-12 22:21:00 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class fikaayo {
+    public static void main(String[] args) {
+    Scanner in= new Scanner (System.in); 
+    System.out.println("Masukkan Nilai: ");
+    int nilai = in.nextInt();
+       if(nilai>=80 && nilai<=89){
+       System.out.println("nilai a");
+       }else{
+       System.out.println("nilai b");
+     }  
+    }
+}
