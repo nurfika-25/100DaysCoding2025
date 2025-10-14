586aab81730b4d8347b8094825cb29daf537405b
nurfika-25 <nurfikaika25@gmail.com>
2025-10-14 20:52:17 +0800

    Create Day37.java

---
+++ Day37.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Fika_20 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int a = in.nextInt();
+        System.out.println();
+        if (a%2 == 0){
+            System.out.println("angka tersebut merupakan bilangan genap");
+        } else {
+            System.out.println("angka tersebut merupakan bilangan ganjil");
+        }
+    }
+}
