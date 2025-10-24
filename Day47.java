4085e1f97af2d225ad1cea9a4d54906b7a8456e4
nurfika-25 <nurfikaika25@gmail.com>
2025-10-24 20:14:28 +0800

    Refactor input handling to use a single Scanner

--- Day47.java
+++ Day47.java
@@ -2,33 +2,31 @@ import java.util.Scanner;
 public class Soal1 {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
-        Scanner input = new Scanner(System.in);
-
         String Senin, Selasa, Rabu, Kamis, Jumat, Sabtu, Minggu;
 
         System.out.println("Masukkan Jadwal Kegiatan Sehari-hari: ");
 
         System.out.print("Senin  : ");
-        Senin = input.nextLine();
+        Senin = in.nextLine();
         System.out.print("Selasa : ");
-        Selasa = input.nextLine();
+        Selasa = in.nextLine();
         System.out.print("Rabu   : ");
-        Rabu = input.nextLine();
+        Rabu = in.nextLine();
         System.out.print("Kamis  : ");
-        Kamis = input.nextLine();
+        Kamis = in.nextLine();
         System.out.print("Jumat  : ");
-        Jumat = input.nextLine();
+        Jumat = in.nextLine();
         System.out.print("Sabtu  : ");
-        Sabtu = input.nextLine();
+        Sabtu = in.nextLine();
         System.out.print("Minggu : ");
-        Minggu = input.nextLine();
+        Minggu = in.nextLine();
 
         System.out.println("Sekarang hari apa? ");
-        String hari = input.nextLine();
+        String Hari = in.nextLine();
 
-        System.out.println("Jadwal kamu hari " + hari + " adalah:");
+        System.out.println("Jadwal kamu hari " + Hari + " adalah:");
 
-        switch (hari) {
+        switch (Hari) {
             case "Senin":
                 System.out.println(Senin);
                 break;
