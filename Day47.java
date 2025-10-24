6e5ef5716d5e04bb7b655c2ec6a3cfda9d030388
nurfika-25 <nurfikaika25@gmail.com>
2025-10-24 20:08:47 +0800

    Refactor daily activity schedule program

--- Day47.java
+++ Day47.java
@@ -1,52 +1,59 @@
 import java.util.Scanner;
-public class Fika_20 {
+public class Soal1 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("===== Kegiatan Sehari-hari =====");
-        System.out.println("Senin.  Bangun tidur, mandi, kuliah, makan, main uno, commit, tidur");
-        System.out.println("Selasa. Bangun tidur, mandi, beres rumah, kuliah, makan, ,commit, tidur");
-        System.out.println("Rabu.   Bangun tidur, mandi, beres-beres rumah, masak, makan, kuliah, commit, tidur");
-        System.out.println("Kamis.  Bangun tidur, mandi, beres-beres rumah, masak, makan, kuliah, kerja kelompok, main uno, commit, tidur");
-        System.out.println("Jumat.  Bangun tidur, mandi, kuliah, makan,kerja kelompok, main uno, commit, tidur");
-        System.out.println("Sabtu.  Bangun tidur, mandi, makan, commit, malming, tidur");
-        System.out.println("Minggu. bangun tidur, mandi, beres-beres rumah, masak, makan, kerja kelompok, commit, tidur");
-        
-        System.out.print("Pilih Hari Senin-Minggu: ");
-        String Hari= in.nextLine();
-        
-        switch (Hari) {
+        Scanner in = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
+
+        String Senin, Selasa, Rabu, Kamis, Jumat, Sabtu, Minggu;
+
+        System.out.println("Masukkan Jadwal Kegiatan Sehari-hari: ");
+
+        System.out.print("Senin  : ");
+        Senin = input.nextLine();
+        System.out.print("Selasa : ");
+        Selasa = input.nextLine();
+        System.out.print("Rabu   : ");
+        Rabu = input.nextLine();
+        System.out.print("Kamis  : ");
+        Kamis = input.nextLine();
+        System.out.print("Jumat  : ");
+        Jumat = input.nextLine();
+        System.out.print("Sabtu  : ");
+        Sabtu = input.nextLine();
+        System.out.print("Minggu : ");
+        Minggu = input.nextLine();
+
+        System.out.println("Sekarang hari apa? ");
+        String hari = input.nextLine();
+
+        System.out.println("Jadwal kamu hari " + hari + " adalah:");
+
+        switch (hari) {
             case "Senin":
-                System.out.println("Kegiatan Anda adalah Bangun tidur, mandi, kuliah, makan, main uno, commit, tidur");
+                System.out.println(Senin);
                 break;
-
             case "Selasa":
-                System.out.println("Kegiatan Anda adalah Bangun tidur, mandi, beres rumah, kuliah, makan, commit, tidur");
+                System.out.println(Selasa);
                 break;
-
             case "Rabu":
-                System.out.println("Kegiatan Anda adalah Bangun tidur, mandi, beres rumah, masak, makan, kuliah, commit, tidur");
+                System.out.println(Rabu);
                 break;
-
             case "Kamis":
-                System.out.println("Kegiatan Anda adalah Bangun tidur, mandi, beres rumah, masak, makan, kuliah, kerja kelompok, main uno, commit, tidur");
+                System.out.println(Kamis);
                 break;
-
             case "Jumat":
-                System.out.println("Kegiatan Anda adalah Bangun tidur, mandi, kuliah, makan, kerja kelompok, main uno, commit, tidur");
+                System.out.println(Jumat);
                 break;
-
             case "Sabtu":
-                System.out.println("Kegiatan Anda adalah Bangun tidur, mandi, makan, commit, malming, tidur");
+                System.out.println(Sabtu);
                 break;
-
             case "Minggu":
-                System.out.println("Kegiatan Anda adalah Bangun tidur, mandi, beres rumah, masak, makan, kerja kelompok, commit, tidur");
+                System.out.println(Minggu);
                 break;
-
             default:
                 System.out.println("Hari yang anda masukkan tidak valid!");
                 break;
         }
+
     }
 }
-  
