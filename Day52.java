1245671f49842832ed7d8c7e45919a9cdb06df09
nurfika-25 <nurfikaika25@gmail.com>
2025-10-29 20:02:47 +0800

    Add Java program to print even numbers from 1 to 20

---
+++ Day52.java
@@ -0,0 +1,14 @@
+public class fikaTernary {
+    public static void main(String[] args) {
+        int angka = 1;
+            System.out.println("Bilangan genap dari 1 sampai 20:");
+                
+            while (angka <= 20) {
+            if (angka % 2 == 0) {
+                System.out.println(angka);
+             }
+            angka++;
+        }
+                System.out.println("Angka Selesai!!!");
+     }
+    }
