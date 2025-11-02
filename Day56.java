17cb097020ba8c85968636ce21bf1bd0e47a0c28
nurfika-25 <nurfikaika25@gmail.com>
2025-11-02 20:51:50 +0800

    Add Soal1 class to print numbers 1 to 5

---
+++ Day56.java
@@ -0,0 +1,10 @@
+public class Soal1 {
+    public static void main(String[] args) {
+     int fi= 1; 
+        do {
+            System.out.println("Angka ke-" + fi);
+            fi++; 
+        } while (fi <= 5);
+    }
+}   
+ 
