5ab5b140f68b76be10bf2d0298db69486222d981
nurfika-25 <nurfikaika25@gmail.com>
2025-11-04 20:52:27 +0800

    Rename Scanner variable for clarity

--- Day58.java
+++ Day58.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class NewClass {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.print("Masukkan nilai N: ");
-        int N = input.nextInt();
+        int N = in.nextInt();
         System.out.println("Angka dari 1 ke " + N + ": ");
         for (int i = 1; i <= N; i++) {
             System.out.print(i + " ");
