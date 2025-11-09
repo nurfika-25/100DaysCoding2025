577a694f5fd812e68c0f53293f5fbb8c7bf78cf5
nurfika-25 <nurfikaika25@gmail.com>
2025-11-09 17:52:00 +0800

    Create Day63.java

---
+++ Day63.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NewClass {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+       System.out.print("Masukkan nilai N:  ");
+        int N = in.nextInt();
+        int hasil = 1;
+  
+          for (int i = 1; i <= N; i++) {
+             hasil *= i;
+        } System.out.println("Hasil perkalian 1 sampai " + N + " adalah: " + hasil);
+    }
+}
