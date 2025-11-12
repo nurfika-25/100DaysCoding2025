80266f5b9bf80614edf347b827a9ff40934b96a0
nurfika-25 <nurfikaika25@gmail.com>
2025-11-12 18:41:46 +0800

    Fix loop structure in PolaVertikalAngka class

---
+++ Day66.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PolaVertikalAngka {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan jumlah baris: ");
+        int baris= in.nextInt();
+
+          for (int i = 1; i <= n; i++) {
+             System.out.println(i);
+        }
+    }
+}
