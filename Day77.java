d499d010881f46e39af670d2ac7dbd0a54e50b66
nurfika-25 <nurfikaika25@gmail.com>
2025-11-23 21:22:56 +0800

    Add Day77.java with string manipulation examples

---
+++ Day77.java
@@ -0,0 +1,13 @@
+package latihansoal;
+import java.util.*;
+public class Latihansoal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String nama = "KOTORSAAAA";
+        System.out.println(nama.substring(5));
+        String kata2 = "apa yang dimaksud dengan laki";
+        System.out.println(kata2.replace("apa", "ini"));
+        String anuu = "      cehh siall sklii halallahh       ";
+        System.out.println(anuu.trim());
+    }
+}
