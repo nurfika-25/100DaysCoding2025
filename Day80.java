cf7b42e111b29262b4f32f2663e9dacea92e8686
nurfika-25 <nurfikaika25@gmail.com>
2025-11-26 18:46:46 +0800

    Fix output formatting in Day80.java

---
+++ Day80.java
@@ -0,0 +1,10 @@
+public class NewClass {
+    public static void main(String[] args) {
+        int [] angka = {10, 20, 30, 40, 50};
+
+            System.out.println("Isi Array:  ");
+            for (int i = 0; i < angka.length; i++) {
+               System.out.println("Indeks " + i + " = " + angka[i]);
+        }
+    }
+}
