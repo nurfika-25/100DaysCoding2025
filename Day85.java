81cf646458fc2a23a884b63391b8ea5b6d0c7b88
nurfika-25 <nurfikaika25@gmail.com>
2025-12-02 20:54:05 +0800

    Update Day85.java

--- Day85.java
+++ Day85.java
@@ -1,13 +1,12 @@
 public class NewClass {
     public static void main(String[] args) {
-        int[] data = {12, 7, 25, 3, 8, 2};
-        int max = data[0]; 
+       int[] data = {12, 7, 25, 3, 8, 2};
+        int max = data[0];
         for (int angka : data) {
             if (angka > max) {
                 max = angka;
             }
         }
-
-        System.out.println("Angka maksimal dari Array adalah:  " + max);
+        System.out.println("Angka maksimal dari Array adalah: " + max);
     }
 }
