5dce6275b8e4537c8d693c8c42121b241d65df01
nurfika-25 <nurfikaika25@gmail.com>
2025-12-04 18:21:38 +0800

    Add MetodAsdos class to display array contents

---
+++ Day88.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MetodAsdos {
+    public static void main(String[] args) {
+        int[] angka = {12, 7, 25, 3, 8, 2};
+        System.out.print ("Isi Array Angka: ");
+        for (int nilai : angka) {
+            System.out.print(nilai +" ");
+        }
+    }
+}
