e5a0a939021295a74e897cd91dbd164665e591df
nurfika-25 <nurfikaika25@gmail.com>
2025-12-05 22:55:42 +0800

    Create MetodAsdos class with fika method
    Add MetodAsdos class with main method and fika method

---
+++ Day89.java
@@ -0,0 +1,13 @@
+package metodasdos;
+import java.util.Scanner;
+public class MetodAsdos {
+    public static void main(String[] args) {
+        fika();
+    }
+    static void fika() {
+        String nama = "Fika";
+        System.out.print("Nama saya adalah: ");
+
+        System.out.print("Umur saya: ");
+    }
+}
