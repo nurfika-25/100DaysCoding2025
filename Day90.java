5997b46a9f53ff160da689d606f2c6ad96a966ac
nurfika-25 <nurfikaika25@gmail.com>
2025-12-06 22:51:27 +0800

    Add MetodAsdos class with area calculation method

---
+++ Day90.java
@@ -0,0 +1,14 @@
+package metodasdos;
+import java.util.Scanner;
+public class MetodAsdos {
+    public static int a (int p, int l){
+        int hasil = p * l;
+        return hasil;
+    }
+    public static void main(String[] args) {
+        MetodAsdos hihi = new MetodAsdos();
+        int hasil = a(3,4);
+        System.out.println("panjang persegi adalah: "+ hasil);      
+
+    }
+}
