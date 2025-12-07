0247d15fe525c5a25d514ac98f6e6f1eaa1c0fdd
nurfika-25 <nurfikaika25@gmail.com>
2025-12-07 21:31:15 +0800

    Implement circle circumference calculation

---
+++ Day91.java
@@ -0,0 +1,10 @@
+package metodasdos;
+import java.util.Scanner;
+public class MetodAsdos {
+    static double a (int r){
+        return 3.14*r*r;
+    }
+    public static void main(String[] args) {
+        System.out.println("Keliling lingkaran adalah: "+a(5));
+    }
+}
