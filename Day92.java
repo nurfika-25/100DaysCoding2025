8b9d23d3844a07ecf56821fb90a6ba8e64d87e73
nurfika-25 <nurfikaika25@gmail.com>
2025-12-08 21:47:43 +0800

    Format multiplication in area calculation method

---
+++ Day92.java
@@ -0,0 +1,10 @@
+package metodasdos;
+import java.util.Scanner;
+public class MetodAsdos {
+    static double a (int a , int t){
+        return 0.5 * a * t;
+    }
+    public static void main(String[] args) {
+        System.out.println("Luas Segitiga adalah: "+a(5,5));
+    }
+}
