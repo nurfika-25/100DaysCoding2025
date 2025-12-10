91f146982283dfd33d6424efdb78bbeae04186c2
nurfika-25 <nurfikaika25@gmail.com>
2025-12-10 23:10:48 +0800

    Add Day94.java with a simple method

---
+++ Day94.java
@@ -0,0 +1,8 @@
+package javaapplication44;
+public class Day93 {
+    public static void main(String[] args) {
+        fikaaa();
+    }static void fikaaa(){
+            System.out.println("Hello Method:)!");
+    }
+}
