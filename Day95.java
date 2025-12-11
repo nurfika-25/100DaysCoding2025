75b229c736af022984edf99b8b1eeb3be3a44c10
nurfika-25 <nurfikaika25@gmail.com>
2025-12-11 23:32:33 +0800

    Create Day95.java

---
+++ Day95.java
@@ -0,0 +1,14 @@
+package javaapplication44;
+
+public class Day93 {
+
+    public static void main(String[] args) {
+        nama("Nurfika", "D0225006");
+    }
+
+    static void nama(String a, String b) {
+
+        System.out.println("Nama  \t: " + a);
+        System.out.println("Nim \t: " + b);
+    }
+}
